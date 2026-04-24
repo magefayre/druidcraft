1e5961e95fde9d5c8840180a5416bc8c21dff67a
Ben Edwards <hello@benedfit.com>
2026-04-24 14:21:11 +0100

    chore: revert

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -35,9 +35,7 @@ const HomePage = ({ beasts }) => (
           <Card
             heading={<h2>{name}</h2>}
             image={{
-              // src: `https://2014.5e.tools/img/bestiary/tokens/${source}/${name}.webp`,
               src: `https://raw.githubusercontent.com/5etools-mirror-3/5etools-2014-img/main/bestiary/${source}/${name}.webp`,
-              // src: `https://www.aidedd.org/dnd/images/${name}.jpg`,
               width: 100,
               height: 100,
               onError: e => (e.target.style.display = 'none')
