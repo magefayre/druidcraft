6f6be633f85b6545652d05ee5d4d9f43f6868af6
Ben Edwards <hello@benedfit.com>
2026-04-29 12:38:54 +0100

    chore: reuse URL function in config

--- next.config.ts
+++ next.config.ts
@@ -2,30 +2,36 @@ import withSvgr from '@newhighsco/next-plugin-svgr'
 import type { NextConfig } from 'next'
 import withPlugins from 'next-compose-plugins'
 
-import { tokenURL } from '~components/Beast/utils'
+import { tokenURL, url } from '~components/Beast/utils'
 
-const BASE = new URL(
-  'https://raw.githubusercontent.com/5etools-mirror-3/5etools-2014-img/main/'
-)
+const BASE = {
+  html: new URL('https://2014.5e.tools/'),
+  img: new URL(
+    'https://raw.githubusercontent.com/5etools-mirror-3/5etools-2014-img/main/'
+  )
+}
 
 const nextConfig: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
-    remotePatterns: [new URL('**', BASE)]
+    remotePatterns: [new URL('**', BASE.img)]
   },
   i18n: { locales: ['en'], defaultLocale: 'en' },
   poweredByHeader: false,
   redirects: () => [
     {
-      source: '/beast/:source/:name',
-      destination: 'https://2014.5e.tools/bestiary/:name-:source.html',
+      source: url(
+        { source: ':source', name: ':name' },
+        { preserveCharacters: [':'] }
+      ),
+      destination: new URL('bestiary/:name-:source.html', BASE.html).href,
       permanent: true
     }
   ],
   rewrites: () => [
     {
       source: tokenURL({ source: ':source', name: ':name' }),
-      destination: new URL('bestiary/tokens/:source/:name.webp', BASE).href
+      destination: new URL('bestiary/tokens/:source/:name.webp', BASE.img).href
     }
   ],
   transpilePackages: ['@newhighsco/chipset', '@newhighsco/press-start'],
--- src/components/Beast/utils.ts
+++ src/components/Beast/utils.ts
@@ -1,12 +1,12 @@
-import slugify from '@sindresorhus/slugify'
+import slugify, { type Options } from '@sindresorhus/slugify'
 import transliterate from '@sindresorhus/transliterate'
 
 import type { Beast } from '~types'
 
-type BasicBeast = Pick<Beast, 'source' | 'name'>
+type BeastURL = Pick<Beast, 'source' | 'name'>
 
-export const tokenURL = ({ source, name }: BasicBeast) =>
+export const tokenURL = ({ source, name }: BeastURL) =>
   `/tokens/${source}/${transliterate(name)}`
 
-export const url = ({ source, name }: BasicBeast) =>
-  `/beast/${slugify(source, { separator: '' })}/${slugify(name)}`
+export const url = ({ source, name }: BeastURL, options?: Options) =>
+  `/beast/${slugify(source, { ...options, separator: '' })}/${slugify(name, options)}`
