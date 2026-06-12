04c5431c655be9d8bb0ce07ec0a036363fb9d3f8
Ben Edwards <hello@benedfit.com>
2026-06-12 17:09:50 +0100

    fix(summon): add meta description

--- src/pages/summon.tsx
+++ src/pages/summon.tsx
@@ -7,7 +7,11 @@ import type { Creature, MonsterType } from '~types'
 import { sortCreatures } from '~utils/creatures'
 import { canonicalUrl } from '~utils/urls'
 
-const meta = { title: 'Summon', canonical: canonicalUrl('summon') }
+const meta = {
+  title: 'Summon',
+  description: 'Never struggle with choosing what to summon again',
+  canonical: canonicalUrl('summon')
+}
 
 type Props = Omit<SummonLayoutProps, 'meta'>
 
