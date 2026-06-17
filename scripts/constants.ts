0e4402513ce7fedbb6004aafbd6a3bd33b3f004e
Ben Edwards <hello@benedfit.com>
2026-06-17 14:07:02 +0100

    chore: try again

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,4 +1,3 @@
 export const BASE = new URL(
-  // renovate: datasource=github-releases depName=5etools-mirror-3/5etools-2014-src
   'https://raw.githubusercontent.com/5etools-mirror-3/5etools-2014-src/refs/tags/v1.217.5/'
 )
