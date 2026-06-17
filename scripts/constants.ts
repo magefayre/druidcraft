bba0f19b5217ed8290bb6bdbb895a9bd61000635
Ben Edwards <hello@benedfit.com>
2026-06-17 13:31:59 +0100

    chore: drop version for testing

--- scripts/constants.ts
+++ scripts/constants.ts
@@ -1,4 +1,4 @@
 export const BASE = new URL(
   // renovate: datasource=github-releases depName=5etools-mirror-3/5etools-2014-src
-  'https://raw.githubusercontent.com/5etools-mirror-3/5etools-2014-src/refs/tags/v1.217.6/'
+  'https://raw.githubusercontent.com/5etools-mirror-3/5etools-2014-src/refs/tags/v1.217.5/'
 )
