d8ce9d7c1450c579c6ce98df0e3918c784247385
Ben Edwards <hello@benedfit.com>
2026-07-23 11:25:57 +0100

    fix: validate urls

---
+++ src/components/Creature/utils.test.ts
@@ -0,0 +1,27 @@
+import { url } from './utils'
+
+describe('url', () => {
+  it('should format the URL as expected', () => {
+    expect(url({ name: 'Aerosaur (Small)', source: 'PSX' })).toEqual(
+      '/creature/psx/aerosaur-small'
+    )
+    expect(url({ name: 'Almiraj', source: 'ToA' })).toEqual(
+      '/creature/toa/almiraj'
+    )
+    expect(url({ name: 'Armored Saber-Toothed Tiger', source: 'CoS' })).toEqual(
+      '/creature/cos/armored-sabertoothed-tiger'
+    )
+    expect(url({ name: 'Awakened Rat', source: 'WDH' })).toEqual(
+      '/creature/wdh/awakened-rat'
+    )
+    expect(url({ name: 'Chimeric Fox', source: 'NRH-AVitW' })).toEqual(
+      '/creature/nrhavitw/chimeric-fox'
+    )
+    expect(url({ name: 'Deep Rothé', source: 'MPMM' })).toEqual(
+      '/creature/mpmm/deep-rothe'
+    )
+    expect(url({ name: 'Giant Fire Beetle', source: 'MM' })).toEqual(
+      '/creature/mm/giant-fire-beetle'
+    )
+  })
+})
