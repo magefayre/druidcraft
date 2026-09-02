affe61a2e5e1d2b9f45b8f10c7d8acb65401baf7
Ben Edwards <hello@benedfit.com>
2026-09-02 10:16:45 +0100

    chore: remove ignore rules

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,4 +1,7 @@
 import config from '@newhighsco/eslint-config'
-import { defineConfig } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([{ extends: [config] }])
+export default defineConfig([
+  { extends: [config] },
+  globalIgnores(['src/types/5etools'])
+])
