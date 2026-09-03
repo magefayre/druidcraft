882c23785181269da6ba0f7b891ebd8c8b4a4a1f
Ben Edwards <hello@benedfit.com>
2026-09-03 16:03:44 +0100

    feat: add dice rollers

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,3 +1,4 @@
+import Button from '~components/Button/Button.module.scss'
 import Card from '~components/Card/Card.module.scss'
 import ContentContainer from '~components/ContentContainer/ContentContainer.module.scss'
 import Grid from '~components/Grid/Grid.module.scss'
@@ -5,4 +6,12 @@ import Icon from '~components/Icon/Icon.module.scss'
 import Navigation from '~components/Navigation/Navigation.module.scss'
 import Tooltip from '~components/Tooltip/Tooltip.module.scss'
 
-export default { Card, ContentContainer, Grid, Icon, Tooltip, Navigation }
+export default {
+  Button,
+  Card,
+  ContentContainer,
+  Grid,
+  Icon,
+  Tooltip,
+  Navigation
+}
